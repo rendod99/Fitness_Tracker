51c671a668f06a6c482f04da5f7a4ac07685fac9
Daniel Rendon <rendod99@gmail.com>
2020-11-17 14:42:57 -0600

    added routes to update a workout and to ad a new workut

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -13,7 +13,30 @@ module.exports = function (app) {
         });
     });
 
-    
+    // adds an exercise
+    app.put("/api/workouts/:id", (req, res) => {
+
+        db.Workout.findOneAndUpdate({ _id: req.params.id },
+            {
+                $push: { exercises: req.body }
+            },
+            { new: true }).then(workouts => {
+                res.json(workouts);
+            }).catch(err => {
+                res.json(err);
+            });
+
+    });
+
+    //creates a new workout
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create(body).then((workouts => {
+            res.json(workouts);
+        })).catch(err => {
+            res.json(err);
+        });
+    });
+
     // get workouts for stats
     app.get("/api/workouts/range", (req, res) => {
 
